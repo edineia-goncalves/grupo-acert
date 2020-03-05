0afc7057dbfa114c3f343c00fd104ba368580bd3
edineia <gedineia80@gmail.com>
2020-03-05 00:02:07 -0300

    adicionado signup

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import * as firebase from 'firebase/app';
+import History from './components/Helpers';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { name: '', email: '' };
+
+        this.handleSignOut = this.handleSignOut.bind(this);
+    }
+
+    componentDidMount() {
+        const user = JSON.parse(localStorage.getItem("user"));
+        this.setState({ ...user });
+    }
+
+    handleSignOut(event) {
+        event.preventDefault();
+        return firebase.auth().signOut().then(() => {
+            localStorage.setItem("user", '')
+            History.push('/login');
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
     render() {
-        return <h1>Hello, Home </h1>;
+        return <div>
+            <span>Olá {this.state.name} -  {this.state.email}</span>
+            <Button
+                onClick={this.handleSignOut}
+                type="submit"
+
+                variant="contained"
+                color="primary"
+            >
+                Sair
+      </Button>
+        </div>
     }
 }
 
