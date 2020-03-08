9f8f022eacc497d6ea12cbb11d77aa8fa075b826
edineia <gedineia80@gmail.com>
2020-03-08 17:35:19 -0300

    adicionado recuperação de senha

--- src/PasswordReset.js
+++ src/PasswordReset.js
@@ -7,42 +7,81 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import TextField from '@material-ui/core/TextField';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
+import * as firebase from 'firebase/app';
 
-export default function PasswordReset(props) {
-    let { show, onClose } = props;
+class PasswordReset extends React.Component {
 
-    return (
-        <Dialog aria-labelledby="simple-dialog-title" open={show} onClose={onClose}>
-            <DialogTitle id="simple-dialog-title">Recuperação de senha</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                    Insira o endereço de e-mail associado à sua conta
-          </DialogContentText>
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    id="email"
-                    label="Email"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                    type="email"
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button size="small" onClick={onClose} color="primary">
-                    Cancelar
-                </Button>
-                <Button size="small" onClick={onClose} s color="primary">
-                    Enviar
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+    constructor(props) {
+        debugger;
+        super(props)
+        this.state = { email: '', errorEmail: false };
+
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.passwordResetEmail = this.passwordResetEmail.bind(this);
+    }
+
+    passwordResetEmail(event) {
+        event.preventDefault();
+        const auth = firebase.auth();
+
+        auth.sendPasswordResetEmail(this.state.email).then((res) => {
+            debugger
+            this.setState({ sendPasswordResetEmail: true });
+            // fechar dialog e mostar msg que o usuário vai receber um e-mail
+        }).catch((error) => {
+            this.setState({ errorEmail: true });
+            console.log(error)
+            // mostrar erro informando que o e-mail não está não foi encontrado na base
+        });
+    }
+
+    handleChangeEmail(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    render() {
+        const { show, onClose } = this.props;
+
+        return (
+            <Dialog aria-labelledby="simple-dialog-title" open={show} onClose={onClose}>
+                <DialogTitle id="simple-dialog-title">Recuperação de senha</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Insira o endereço de e-mail associado à sua conta
+                    </DialogContentText>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        name="email"
+                        autoComplete="email"
+                        value={this.state.email}
+                        onChange={this.handleChangeEmail}
+                        autoFocus
+                        type="email"
+                        error={this.errorEmail}
+                        helperText={this.errorEmail ? 'E-mail não encontrado' : ' '}
+                    >
+                    </TextField>
+                </DialogContent>
+                <DialogActions>
+                    <Button size="small" onClick={onClose} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button size="small" onClick={this.passwordResetEmail} color="primary">
+                        Enviar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
 }
 
 PasswordReset.propTypes = {
     onClose: PropTypes.func.isRequired,
     show: PropTypes.bool.isRequired,
 };
+
+export default PasswordReset;
