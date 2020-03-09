6d958eba7ddd4bc2ca06ff77465e567afa91453f
edineia <gedineia80@gmail.com>
2020-03-09 20:41:00 -0300

    adicionado pesquisa por artista

---
+++ src/Home/Artists.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Search from '../components/Search';
+import Api from "../Services/Artists";
+import BarGridList from "../components/GridList"
+
+class Artists extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { items: [] };
+
+        this.searchArtists = this.searchArtists.bind(this);
+    }
+
+    searchArtists(term) {
+        Api.getByArtist(term).then(({ results }) => {
+            const data = results && results.artistmatches && results.artistmatches.artist
+
+            if (!data) return
+
+            const list = data.map(artist => {
+                return {
+                    name: artist.name,
+                    listeners: artist.listeners,
+                    url: artist.url,
+                    image: Object.values(artist.image[0], "#text")
+                }
+            })
+            this.setState({
+                items: [...list]
+            })
+        }).catch((error) => error && this.setState({
+            items: []
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <Search placeholder="Digite o nome do artista e aperte enter para pesquisar..."
+                    handleKeyPress={this.searchArtists}
+                />
+                <React.Fragment>
+                    <BarGridList data={this.state.items}></BarGridList>
+                </React.Fragment>
+            </div>
+        )
+    }
+}
+
+export default Artists;
---
+++ src/components/GridList.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+        width: "70%",
+        height: "100%",
+    },
+    icon: {
+        color: 'rgba(255, 255, 255, 0.54)',
+    },
+}));
+
+export default function BarGridList(props) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <GridList cellHeight={180} className={classes.gridList}>
+
+                {props.data.map(tile => (
+                    <GridListTile key={`${tile}-${tile.name}`}>
+                        <img src={tile.image[0]} alt={tile.name} />
+                        <GridListTileBar
+                            title={tile.name}
+                            subtitle={<span>{tile.listeners}</span>}
+                            actionIcon={
+                                <IconButton aria-label={`info about ${tile.name}`} className={classes.icon}>
+                                    <InfoIcon />
+                                </IconButton>
+                            }
+                        />
+                    </GridListTile>
+                ))}
+            </GridList>
+        </div>
+    );
+}
